b9a1628c5e232133de371c1d32ca230c0394678a
HitBlast <hitblastlive@gmail.com>
2022-03-06 11:19:22 +0600

    Updated views.

--- main.py
+++ main.py
@@ -280,14 +280,14 @@ class HelpCommandDropdown(disnake.ui.Select):
 
 # Views (static).
 class VoteCommandView(disnake.ui.View):
-    def __init__(self, timeout: float=30):
+    def __init__(self, timeout: float=10):
         super().__init__(timeout=timeout)
 
         self.add_item(disnake.ui.Button(label='Vote Now', url='https://top.gg/bot/867998923250352189/vote'))
         self.add_item(disnake.ui.Button(label='Website', url='https://hitblast.github.io/Veron1CA'))
 
 class HelpCommandView(disnake.ui.View):
-    def __init__(self, timeout: float=30):
+    def __init__(self, timeout: float=10):
         super().__init__(timeout=timeout)
 
         self.add_item(HelpCommandDropdown())
@@ -1941,7 +1941,7 @@ def get_queue_embed(ctx: commands.Context, page: int=1):
 
 # Views (static / dynamic, for music commands).
 class NowCommandView(disnake.ui.View):
-    def __init__(self, *, url: str, views: str, likes: str, timeout: float=30):
+    def __init__(self, url: str, views: str, likes: str, timeout: float=10):
         super().__init__(timeout=timeout)
 
         self.add_item(disnake.ui.Button(label='Redirect', url=url))
@@ -1949,7 +1949,7 @@ class NowCommandView(disnake.ui.View):
         self.add_item(disnake.ui.Button(label=f'{int(likes):,} Likes', style=disnake.ButtonStyle.grey))
 
 class PlayCommandView(disnake.ui.View):
-    def __init__(self, *, url: str, timeout: float=30):
+    def __init__(self, url: str, timeout: float=10):
         super().__init__(timeout=timeout)
     
         self.add_item(disnake.ui.Button(label='Redirect', url=url))
@@ -1963,10 +1963,8 @@ class QueueView(disnake.ui.View):
     async def clear(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
         self.ctx.voice_state.songs.clear()
         await interaction.response.edit_message(
-            embed=disnake.Embed(
-                title='The queue has been cleared.',
-                color=accent_color['primary']
-            ),
+            content='The queue has been cleared.',
+            embed=None,
             view=None
         )
         await interaction.response.delete(delay=5)
