b410c77182fcaded34cb34bcea92cf1206d6c19a
HitBlast <hitblastlive@gmail.com>
2022-03-08 21:26:41 +0600

    Commented volume limitations for a testing purpose

--- main.py
+++ main.py
@@ -2226,8 +2226,8 @@ class Music(commands.Cog):
                 )
                 return await ctx.reply(embed=embed)
 
-            if not 0 < volume <= 100:
-                return await ctx.reply('Volume must be between 1 and 100 to execute the command.')
+            # if not 0 < volume <= 100:
+                # return await ctx.reply('Volume must be between 1 and 100 to execute the command.')
 
             ctx.voice_state.current.source.volume = volume / 100
             await ctx.reply(f'Volume of the player is now set to **{volume}%**')
