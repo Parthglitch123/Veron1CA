bc8fabbc29280928f73cd3a92379898748f9eb0f
HitBlast <hitblastlive@gmail.com>
2022-03-19 21:52:34 +0600

    Added a `timeout` param to `PlayCommandView`

--- main.py
+++ main.py
@@ -1975,16 +1975,14 @@ class NowCommandView(disnake.ui.View):
             await interaction.send(embed=embed, view=VoteCommandView())
 
 class PlayCommandView(disnake.ui.View):
-    def __init__(self, url: str):
+    def __init__(self, url: str, timeout: float=10):
+        super().__init__(timeout=timeout)
         self.add_item(disnake.ui.Button(label='Redirect', url=url))
 
 class QueueCommandView(disnake.ui.View):
     def __init__(self, ctx: commands.Context, timeout: float=50):
         super().__init__(timeout=timeout)
         self.ctx = ctx
-
-    async def on_timeout(self):
-        pass
 
     @disnake.ui.button(label='Clear Queue', style=disnake.ButtonStyle.danger)
     async def clear(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
