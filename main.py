7faad61ba3ca351f584faa2d2a4db54e5d2f67a0
HitBlast <hitblastlive@gmail.com>
2022-03-19 22:05:07 +0600

    Returned `timeout` param for `VoteCommandView`

--- main.py
+++ main.py
@@ -301,7 +301,9 @@ class HelpCommandDropdown(disnake.ui.Select):
 
 # Views (static).
 class VoteCommandView(disnake.ui.View):
-    def __init__(self):
+    def __init__(self, timeout: float=10):
+        super().__init__(timeout=timeout)
+        
         self.add_item(disnake.ui.Button(label='Vote Now', url='https://top.gg/bot/867998923250352189/vote'))
         self.add_item(disnake.ui.Button(label='Website', url='https://hitblast.github.io/Veron1CA'))
 
